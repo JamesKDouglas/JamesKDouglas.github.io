c1e8aeee44000b19612f3ad51142b2b5cf5fc428
James Douglas <jamesdouglas@Jamess-MacBook-Air.local>
2024-12-05 16:57:34 -0800

    clean up old functions and comments

--- groundUp/spectrogram.js
+++ groundUp/spectrogram.js
@@ -28,10 +28,7 @@ function getData() {
             let WIDTH = window.innerWidth;
             let HEIGHT = window.innerHeight;
 
-            //width can be handled better with CSS, either with as % or vw.
             canvas.width = WIDTH;
-
-            //But height defines how many lines are created before starting in the top, and that's right here in js.
             canvas.height = HEIGHT;
             offScreenCanvas.width = WIDTH;
             offScreenCanvas.height = HEIGHT;
@@ -101,14 +98,8 @@ function getData() {
             let rgbAll = makeRGBArr().reverse();
             function drawSpectrogram() {
 
-                //right now the scale isn't logarithmic and I'd like to change that. The high frequencies get a lot of space even though we can't hear them very well.
-                //There is a notch at 13000hz, but spectroid has that too. It just isn't as obvious because of the scale.
-                //Also it's black and white, which is also nice but not as psychedelic as the blue to pink. What color space even is that? Viridis magma? I may simply map 240 colors in an array but I still need to make that somehow.
-                //I could see a curve going through the lefthand rectangular photo here, https://en.wikipedia.org/wiki/HSL_and_HSV#/media/File:Hsl-hsv_models.svg
-                //Or from the white then following near the edge towards the pink corner, swooping towards the blue and up towards black corner
-                https://www.infinityinsight.com/product.php?id=91
-                //That is, the first part is G decreasing, second. Then R and G decrease together to make the turn, then R decreases straight for a while. Another turn where R and B decrease. Finally, blue decreases. That would take us from white through pink, into the dark blues and black. Just divide this journey into about 250 pieces. 70 20 70 20 70 comes to 250.
-                //Also I would like a scale.
+                //right now the scale isn't logarithmic and I could change that. That was one of the original specifications, but honestly I like it how it is. 
+                //There is a notch at 13000hz, but (the inspiration for this) spectroid has that too. It just isn't as obvious because of the scale.
                 requestWakeLock();
                 requestAnimationFrame(drawSpectrogram);
                 analyser.getFloatFrequencyData(dataArray);
@@ -132,7 +123,7 @@ function getData() {
                     offScreenCtx.putImageData(imageData, 0, yOffset);
                 }
                 else {
-                    //This just paints the scale at the top so I can see it/work with it.
+                    //This just paints the scale at the top.
                     for (let i = 0; i < bufferLength; i++) {
                         let intensity = Math.round((i/bufferLength)*249);
                         let index = i * 4;
@@ -154,62 +145,6 @@ function getData() {
             }
             drawSpectrogram();
             releaseWakeLock();
-            // let counter = 0;
-            function drawWave() {
-                // if (counter == 100){
-                //     console.log(dataArray);
-                //     counter = 0;
-                // }
-                // counter++;
-                requestAnimationFrame(drawWave);
-                analyser.getFloatTimeDomainData(dataArray);
-                canvasCtx.fillStyle = "rgb(200 200 200)";
-                canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-                canvasCtx.lineWidth = 2;
-                canvasCtx.strokeStyle = "rgb(0 0 0)";
-                canvasCtx.beginPath();
-                const sliceWidth = WIDTH / bufferLength;
-                let x = 0;
-                for (let i = 0; i < bufferLength; i++) {
-                    const v = (dataArray[i] + 1.0) / 2.0;
-                    const y = v * HEIGHT;
-                  
-                    if (i === 0) {
-                      canvasCtx.moveTo(x, y);
-                    } else {
-                      canvasCtx.lineTo(x, y);
-                    }
-                  
-                    x += sliceWidth;
-                  }
-                canvasCtx.lineTo(WIDTH, HEIGHT / 2);
-                canvasCtx.stroke();
-            }
-            // drawWave();
-
-            function drawFft(){
-                requestAnimationFrame(drawFft);
-                analyser.getFloatTimeDomainData(dataArray);
-
-                canvasCtx.fillStyle = "rgb(200 200 200)";
-                canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-                const barWidth = (WIDTH / bufferLength) * 2.5;
-                let barHeight;
-                let x = 0;
-
-                for (let i = 0; i < bufferLength; i++) {
-                    // barHeight = dataArray[i] / 2;
-                    barHeight = (dataArray[i] + 1) * (HEIGHT / 2);
-
-                    canvasCtx.fillStyle = `rgb(${barHeight + 100} 50 50)`;
-                    canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
-                  
-                    x += barWidth + 1;
-                }
-            }
-            // drawFft();
-
             
         })
         .catch(err => {
